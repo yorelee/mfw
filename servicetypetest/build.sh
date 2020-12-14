9c349ba5e26fe3dd866dd7bfb30f0a3b418f3af9
mfw <modern-skcc@sk.com>
2020-12-14 07:49:37 +0000

    This is modern framework base code template.

---
+++ servicetypetest/build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+mvn clean install -DskipTests=true && 
+docker build --tag vup-registry.cloudzcp.io/modern/example/servicetypetest:0.0.1 . && 
+docker push vup-registry.cloudzcp.io/modern/example/servicetypetest:0.0.1
